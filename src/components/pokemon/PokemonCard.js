d64df357416c5bcffb3386aa21a97f41296af945
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-01-07 14:51:50 -0300

    Request data from the PokeApi and display on the app

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,13 +1,57 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const Sprite = styled.img`
+  width: 5em;
+  height: 5em;
+  display: none;
+`;
 
 class PokemonCard extends Component {
+  state = {
+    name: "",
+    imageUrl: "",
+    pokemonIndex: "",
+    imageLoading: true
+  };
+
+  getImageUrl(index) {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`;
+  }
+
+  componentDidMount() {
+    const { name, url } = this.props;
+    const pokemonIndex = url.split("/")[6];
+    const imageUrl = this.getImageUrl(pokemonIndex);
+
+    this.setState({
+      name,
+      imageUrl,
+      pokemonIndex
+    });
+  }
+
   render() {
     return (
       <div className="col-md-3 col-sm-6 mb-5">
         <div className="card">
-          <div className="card-header">
-            <h1>Pokemon Card</h1>
+          <h5 className="card-header text-capitalize">
+            <b>{this.state.pokemonIndex + " "}</b>
+            {this.state.name}
+          </h5>
+
+          <div className="card-body mx-auto">
+            <Sprite
+              className="card-img-top rounded mx-auto mt-2 "
+              onLoad={() => this.setState({ imageLoading: false })}
+              src={this.state.imageUrl}
+              style={this.state.imageLoading ? null : { display: "block" }}
+            ></Sprite>
           </div>
+
+          {this.state.imageLoading ? (
+            <div class="spinner-border mx-auto spinner"></div>
+          ) : null}
         </div>
       </div>
     );
