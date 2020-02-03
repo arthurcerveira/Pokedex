778b3c209986b738e1f40392708bfd76505deb71
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-02-03 18:29:54 -0300

    Fix card shadow

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -12,7 +12,7 @@ const Card = styled.div`
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   &:hover {
-    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.21);
+    box-shadow: 0 7px 10px rgba(0, 0, 0, 0.25), 0 5px 5px rgba(0, 0, 0, 0.21);
   }
   -moz-user-select: none;
   -website-user-select: none;
