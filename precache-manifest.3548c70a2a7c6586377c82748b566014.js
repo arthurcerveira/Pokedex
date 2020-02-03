f1dcbb41f015e00a252cec35adae202c42b5ef41
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-02-03 18:30:39 -0300

    Updates

---
+++ precache-manifest.3548c70a2a7c6586377c82748b566014.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b040bd920ee195974b38b44f7f498382",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "ea0ff1e319c53566a9fe",
+    "url": "/Pokedex/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "80df74f49f9aea328ea5",
+    "url": "/Pokedex/static/css/main.49d32df5.chunk.css"
+  },
+  {
+    "revision": "ea0ff1e319c53566a9fe",
+    "url": "/Pokedex/static/js/2.b3a42b24.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/Pokedex/static/js/2.b3a42b24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80df74f49f9aea328ea5",
+    "url": "/Pokedex/static/js/main.fbab0fba.chunk.js"
+  },
+  {
+    "revision": "631f8551bc29cb0a1cdc",
+    "url": "/Pokedex/static/js/runtime-main.8225d4fe.js"
+  }
+]);
